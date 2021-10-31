f9bf5d3f2e4508abae6f3516ed81ca70e6305622
p0pkern <petermanchristoph@gmail.com>
2021-10-31 13:17:00 -0400

    Sql server and image request ready to go, waiting on update from image server

--- app.js
+++ app.js
@@ -1,22 +1,161 @@
+const vote = (voteOn, amount) => {
+    /*
+        Increase and decrease the votes by an increment of 1 or -1
+        Text on screen is updated before database is updated
+    */
+
+
+    // Increase/decrease the votes on the page first
+    const newVote = Number(document.getElementById(voteOn).innerText) + Number(amount);
+    document.getElementById(voteOn).innerText = newVote
+
+    // Pull the inner text from all the votes and send them over the mysql database
+    const championNewVotes = document.getElementById('votes-new').innerText;
+    const challengerOneVotes = document.getElementById('votes-1').innerText;
+    const challengerTwoVotes = document.getElementById('votes-2').innerText;
+
+    const body = {
+        "championNewVotes" : championNewVotes,
+        "challengerOneVotes" : challengerOneVotes,
+        "challengerTwoVotes" : challengerTwoVotes
+    }
+
+    // Update the vote contents of the mysql database and reload the page
+    const req = new XMLHttpRequest();
+    req.open('POST', "http://flip1.engr.oregonstate.edu:2173/vote", true)
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.send(JSON.stringify(body));
+    req.onload = function() {
+        if (req.readyState == 4 && req.status <= 200 && req.status < 400) {
+            updateAll();
+            
+        } else {
+            console.log(req.responseText);
+        }
+    }
+}
+
+const totalVotes = () => {
+    /*
+        Totals the votes of all the images and shuffles or replaces the images as needed
+    */
+    const championNewVotes = Number(document.getElementById('votes-new').innerText);
+    const challengerOneVotes = Number(document.getElementById('votes-1').innerText);
+    const challengerTwoVotes = Number(document.getElementById('votes-2').innerText);
+
+    if (challengerOneVotes > championNewVotes && challengerOneVotes > challengerTwoVotes) {
+        changeWinner('votes-1', 'img-two');
+    } else if (challengerTwoVotes > championNewVotes && challengerTwoVotes > challengerOneVotes) {
+        changeWinner('votes-2', 'img-three');
+    } else {
+        changeWinner('votes-new', 'img-one');
+    }
+}
+
+const changeWinner = (winner, winImg) => {
+    /*
+        If the champion loses, we will replace the champion data with the new winner
+    */
+   const championVotes = document.getElementById(winner).innerText;
+   const championImg = document.getElementById(winImg).getAttribute('src');
+
+   const body = {
+    "championVotes" : championVotes,
+    "championImg" : championImg
+   }
+
+   console.log(body)
+
+   changeChallengers(body);
+
+}
+
+const changeChallengers = (body) => {
+   const challengerOneImg =  getNewImage();
+   const challengerTwoImg =  getNewImage();
+
+   body["championNewVotes"] = 0;
+   body["challengerOneImg"] = challengerOneImg;
+   body["challengerOneVotes"] = 0;
+   body["challengerTwoImg"] = challengerTwoImg,
+   body["challengerTwoVotes"] = 0;
+
+   console.log(body)
+
+   const req = new XMLHttpRequest();
+   req.open("POST", "http://flip1.engr.oregonstate.edu:2173/finishBattle", false);
+   req.setRequestHeader('Content-Type', 'application/json');
+   req.send(JSON.stringify(body));
+   req.onload = function() {
+    if (req.readyState == 4 && req.status <= 200 && req.status < 400) {
+        updateAll();
+    } else {
+        console.log(req.responseText);
+    }
+}
+}
+
+const getNewImage = () => {
+    let response;
+
+    const req = new XMLHttpRequest();
+    req.open("GET", "http://flip3.engr.oregonstate.edu:17778/getImage?response_type=link", false);
+    req.onload = function() {
+        if (req.readyState == 4 && req.status >= 200 && req.status < 400) {
+            // const payLoad = JSON.parse(req.responseText);
+            response = JSON.parse(req.responseText);
+            
+        } else {
+            console.log(req.statusText);
+        }
+    }
+    req.send(null);
+    return response;
+}
+
+
 const updateAll = () => {
     /*
         Pulls the images and votes currently in the database and populates them into
         their fields.
     */
 
+    let votesPrev = document.getElementById("votes-prev");
+    let votesNew = document.getElementById("votes-new");
+    let votesOne = document.getElementById("votes-1");
+    let votesTwo = document.getElementById("votes-2");
+    let imgOne = document.getElementById("img-one");
+    let imgTwo = document.getElementById("img-two");
+    let imgThree = document.getElementById("img-three");
+
+
     const req = new XMLHttpRequest();
     req.open("GET", "http://flip1.engr.oregonstate.edu:2173/", false);
     req.onload = function() {
         if (req.readyState == 4 && req.status >= 200 && req.status < 400) {
             const payLoad = JSON.parse(req.responseText);
-            document.getElementById("votes-prev").innerText = payLoad['rows'][0]['championVotes'];
-            document.getElementById("votes-new").innerText = payLoad['rows'][0]['championNewVotes'];
-            document.getElementById("votes-1").innerText = payLoad['rows'][0]['challengerOneVotes'];
-            document.getElementById("votes-2").innerText = payLoad['rows'][0]['challengerTwoVotes'];
-            document.getElementById("img-one").setAttribute('src', payLoad['rows'][0]['championImg']);
-            document.getElementById("img-two").setAttribute('src', payLoad['rows'][0]['challengerOneImg']);
-            document.getElementById("img-three").setAttribute('src', payLoad['rows'][0]['challengerTwoImg']);
 
+            if (votesPrev.innerText != payLoad['rows'][0]['championVotes']) {
+                votesPrev.innerText = payLoad['rows'][0]['championVotes'];
+            }
+            if (votesNew.innerText != payLoad['rows'][0]['championNewVotes']) {
+                votesNew.innerText = payLoad['rows'][0]['championNewVotes'];
+            }
+            if (votesOne.innerText != payLoad['rows'][0]['challengerOneVotes']) {
+                votesOne.innerText = payLoad['rows'][0]['challengerOneVotes'];
+            }
+            if (votesTwo.innerText != payLoad['rows'][0]['challengerTwoVotes']) {
+                votesTwo.innerText = payLoad['rows'][0]['challengerTwoVotes'];
+            }
+            if (imgOne.getAttribute('src') != payLoad['rows'][0]['championImg']) {
+                imgOne.setAttribute('src', payLoad['rows'][0]['championImg']);
+            }
+            if (imgTwo.getAttribute('src')  != payLoad['rows'][0]['challengerOneImg']) {
+                imgTwo.setAttribute('src', payLoad['rows'][0]['challengerOneImg']);
+            }
+            if (imgThree.getAttribute('src')  != payLoad['rows'][0]['challengerTwoImg']) {
+                imgThree.setAttribute('src', payLoad['rows'][0]['challengerTwoImg']);
+            }
             
         } else {
             console.error(payLoad);
---
+++ dbaseQuery.js
@@ -0,0 +1,136 @@
+const mysql = require('./dbcon.js');
+const express = require('express');
+const CORS = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.set('port', 2173);
+app.use('/public_html', express.static('./public_html/'));
+app.use(CORS());
+
+// Query parameters
+const finishBattle = "UPDATE battleImages SET championVotes=?, championImg=?, championNewVotes=?, challengerOneImg=?, challengerOneVotes=?, challengerTwoImg=?, challengerTwoVotes=? WHERE id=? ";
+const genericQuery = "SELECT * FROM battleImages";
+const voteQuery = "UPDATE battleImages SET championNewVotes=?, challengerOneVotes=?, challengerTwoVotes=? WHERE id=?";
+
+// These are just for troubleshooting/setup purposes
+const dropTableQuery = "DROP TABLE IF EXISTS battleImages";
+const makeTableQuery = `CREATE TABLE battleImages (
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        championImg VARCHAR(2083) NOT NULL,
+                        championVotes INT,
+                        championNewVotes INT,
+                        challengerOneImg VARCHAR(2083) NOT NULL,
+                        challengerOneVotes INT,
+                        challengerTwoImg VARCHAR(2083) NOT NULL,
+                        challengerTwoVotes INT);`;
+const insertQuery = "INSERT INTO battleImages (`championImg`, `championVotes`, `championNewVotes`, `challengerOneImg`, `challengerOneVotes`, `challengerTwoImg`, `challengerTwoVotes`) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+
+// Get request for all data
+const getAllData = (res) => {
+    mysql.pool.query(genericQuery, (err, rows, fields) => {
+      if(err){
+        next(err);
+        return;
+      }
+      res.json({"rows" : rows});
+    })
+  }
+
+// Retrieve all current data in the database
+app.get('/', function(req, res, next) {
+    getAllData(res)
+})
+
+// Update the votes for all of the images in the database
+app.post('/vote', function(req, res, next) {
+  const {championNewVotes, challengerOneVotes, challengerTwoVotes} = req.body;
+
+  mysql.pool.query(voteQuery, [championNewVotes, challengerOneVotes, challengerTwoVotes, 1], (err, results) => {
+    if(err){
+      next(err);
+      return;
+    }
+    getAllData(res);
+  })
+});
+
+app.post('/finishBattle', function(req, res, next) {
+  // championVotes=?, championImg=?, championNewVotes=?, challengerOneImg=?, challengerOneVotes=?, challengerTwoImg=?, challengerTwoVotes=?
+  const {championVotes, championNewVotes, championImg, challengerOneImg, challengerOneVotes, challengerTwoImg, challengerTwoVotes} = req.body;
+
+  mysql.pool.query(finishBattle, [championVotes, championImg, championNewVotes, challengerOneImg, challengerOneVotes, challengerTwoImg, challengerTwoVotes, 1], (err, results) =>{
+    if(err){
+      next(err);
+      return;
+    }
+    getAllData(res);
+  })
+})
+
+// TROUBLE SHOOTING TO BE DELETED
+// Reset the table to empty values for troubleshooting
+app.get('/reset-table',function(req,res,next){
+    mysql.pool.query(dropTableQuery, function(err){
+      mysql.pool.query(makeTableQuery, function(err){
+        const championImg = "https://image.shutterstock.com/image-vector/random-blotch-inkblot-organic-blob-600w-1612925449.jpg";
+        const championVotes = 0;
+        const championNewVotes = 0;
+        const challengerOneImg = "https://image.shutterstock.com/image-vector/mystery-box-random-loot-flat-600w-1469820695.jpg";
+        const challengerOneVotes = 0;
+        const challengerTwoImg = "https://www.kenyons.com/wp-content/uploads/2017/04/default-image.jpg";
+        const challengerTwoVotes = 0;
+    
+        mysql.pool.query(insertQuery, 
+            [championImg, championVotes, championNewVotes, challengerOneImg, challengerOneVotes, challengerTwoImg, challengerTwoVotes], 
+            (err, result) => {
+            if(err){
+                next(err);
+                return;
+            }
+            getAllData(res);
+        })
+        
+      })
+    });
+  });
+
+app.get('/update', function(req, res, next) {
+    
+    const idRow = 1;
+    const championImg = "";
+    const championVotes = 4;
+    const championNewVotes = 0;
+    const challengerOneImg = "";
+    const challengerOneVotes = 0;
+    const challengerTwoImg = "";
+    const challengerTwoVotes = 0;
+    
+
+    mysql.pool.query(finishBattle, [championImg, championVotes, championNewVotes, challengerOneImg, challengerOneVotes, challengerTwoImg, challengerTwoVotes, idRow], 
+        (err, result) => {
+            if(err){
+                next(err);
+                return;
+            }
+            getAllData(res);
+    })
+})
+
+// Error responses
+app.use(function(req,res){
+    res.status(404);
+    console.log('404');
+  });
+  
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    console.log(err.status);
+  });
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://flip1.engr.oregonstate.edu:' + app.get('port') + "; Press Ctrl-C to quit.")
+})
